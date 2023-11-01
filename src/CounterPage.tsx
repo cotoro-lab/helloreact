06ad7e12891978a20e668438bc862faafc0fb175
cotoro <shuta.u824@gmail.com>
2023-11-02 01:09:59 +0900

    Routing use 'react-router-dom'

---
+++ src/CounterPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const CounterPage = () => {
+    return (
+        <div>
+            <h1>Counter Page</h1>
+        </div>
+
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import { RouterConfig } from './RouterConfig';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <React.StrictMode>
+  <RouterConfig />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
